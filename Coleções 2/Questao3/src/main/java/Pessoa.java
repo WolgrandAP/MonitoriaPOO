8b894b0469b464aa75ab62ddc8712e018e4082f3
Wolgrand <wolgrand.tecdev@gmail.com>
2025-05-16 12:40:13 -0300

    Q3

---
+++ Coleções 2/Questao3/src/main/java/Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private boolean prioridade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(boolean prioridade) {
+        this.prioridade = prioridade;
+    }
+
+}
