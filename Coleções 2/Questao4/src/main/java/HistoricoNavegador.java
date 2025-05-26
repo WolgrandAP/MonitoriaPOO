2a4e2b898b73f37dfe9851427b7dff2145064c72
Wolgrand <wolgrand.tecdev@gmail.com>
2025-05-26 10:50:17 -0300

    Questão 4 incompleta

--- Coleções 2/Questao4/src/main/java/HistoricoNavegador.java
+++ Coleções 2/Questao4/src/main/java/HistoricoNavegador.java
@@ -2,8 +2,22 @@ import java.util.Stack;
 
 public class HistoricoNavegador {
 
-    private Stack<Pagina> pilha = new Stack<>();
+    private Stack<String> pilha = new Stack<>();
 
+    public void novaPagina(String url) {
+        pilha.push(url);
+    }
 
+    public void removerPagina() {
+        pilha.pop();
+    }
+
+    public String retornarPagina() {
+        return pilha.peek();
+    }
+
+    public boolean pilhacomElemento() {
+        return !pilha.isEmpty();
+    }
 
 }
--- Coleções 2/Questao4/src/main/java/Main.java
+++ Coleções 2/Questao4/src/main/java/Main.java
@@ -1,10 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        PaginasDados p = new PaginasDados();
+        HistoricoNavegador pilhaInicial = new HistoricoNavegador();
+        HistoricoNavegador pilhaFinal = new HistoricoNavegador();
 
-        System.out.println(p.p1.getUrl());
+        Scanner tec = new Scanner(System.in);
+        int resp = 10;
+
+        System.out.println("Qual será sua ação: \n1 - Visitar uma nova página \n2 - Voltar para a página anterior \n3 - Avançar para a próxima página \n0 - Sair");
+        while (resp != 0) {
+            System.out.print("Digite o número:");
+            resp = tec.nextInt();
+            tec.nextLine();
+
+            switch (resp) {
+                case(1) :
+                    System.out.print("Digite a url da página: ");
+                    String pag = tec.nextLine();
+                    pilhaInicial.novaPagina(pag);
+                    break;
+
+                case(2) :
+                    if (pilhaInicial.pilhacomElemento()) {
+                        String pagAtual = pilhaInicial.retornarPagina();
+                        pilhaInicial.removerPagina();
+                        pilhaFinal.novaPagina(pagAtual);
+                        System.out.println("Página: " + pilhaInicial.retornarPagina());
+                    } else {
+                        System.out.println("Não há mais páginas anteriores, vá para a próxima adiante.");
+                    }
+                    break;
+
+                case(3):
+                    if (pilhaFinal.pilhacomElemento()) {
+                        String proxPag = pilhaFinal.retornarPagina();
+                        pilhaFinal.removerPagina();
+                        pilhaInicial.novaPagina(proxPag);
+                        System.out.println("Página: " + pilhaInicial.retornarPagina());
+                    } else {
+                        System.out.println("Não há mais páginas adinate, volte a páina uma adicione uma nova.");
+                    }
+                    break;
+            }
+
+        }
 
     }
 
